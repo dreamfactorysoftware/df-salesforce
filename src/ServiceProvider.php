e07fc52774e0087f36047c6dc9f254032ec65e95
Lee Hicks <leehicks@dreamfactory.com>
2016-08-19 16:06:31 -0400

    DF-607 #resolve #comment Making service docs always viewable, even auto-generated ones.

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,6 +1,7 @@
 <?php
 namespace DreamFactory\Core\Salesforce;
 
+use DreamFactory\Core\Components\ServiceDocBuilder;
 use DreamFactory\Core\Enums\ServiceTypeGroups;
 use DreamFactory\Core\Salesforce\Models\SalesforceConfig;
 use DreamFactory\Core\Salesforce\Services\SalesforceDb;
@@ -9,18 +10,23 @@ use DreamFactory\Core\Services\ServiceType;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
+    use ServiceDocBuilder;
+
     public function register()
     {
         // Add our service types.
-        $this->app->resolving('df.service', function (ServiceManager $df){
+        $this->app->resolving('df.service', function (ServiceManager $df) {
             $df->addType(
                 new ServiceType([
-                    'name'           => 'salesforce_db',
-                    'label'          => 'Salesforce',
-                    'description'    => 'Database service for Salesforce connections.',
-                    'group'          => ServiceTypeGroups::DATABASE,
-                    'config_handler' => SalesforceConfig::class,
-                    'factory'        => function ($config){
+                    'name'            => 'salesforce_db',
+                    'label'           => 'Salesforce',
+                    'description'     => 'Database service for Salesforce connections.',
+                    'group'           => ServiceTypeGroups::DATABASE,
+                    'config_handler'  => SalesforceConfig::class,
+                    'default_api_doc' => function ($service) {
+                        return $this->buildServiceDoc($service->id, SalesforceDb::getApiDocInfo($service));
+                    },
+                    'factory'         => function ($config) {
                         return new SalesforceDb($config);
                     },
                 ])
